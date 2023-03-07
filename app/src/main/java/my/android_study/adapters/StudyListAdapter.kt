8893fd6fbdf330a945a33c0cf3faee81d69c2271
JAEHONG LEE <tvroom88@naver.com>
2023-03-07 17:21:28 +0900

    Add WebView

--- app/src/main/java/my/android_study/adapters/StudyListAdapter.kt
+++ app/src/main/java/my/android_study/adapters/StudyListAdapter.kt
@@ -1,25 +1,35 @@
 package my.android_study.adapters
 
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import my.android_study.R
+import my.android_study.WebViewActivity
+import my.android_study.data.AndroidStudyList
 import my.android_study.models.AndroidStudyItem
 
-class StudyListAdapter(private val dataSet: ArrayList<AndroidStudyItem>) :
-    RecyclerView.Adapter<StudyListAdapter.ViewHolder>() {
 
-    /**
-     * Provide a reference to the type of views that you are using
-     * (custom ViewHolder).
-     */
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+class StudyListAdapter() : RecyclerView.Adapter<StudyListAdapter.ViewHolder>() {
+    val dataSet = AndroidStudyList.androidStudyList
+
+    class ViewHolder(view: View, dataSet: List<AndroidStudyItem>) : RecyclerView.ViewHolder(view) {
         val textView: TextView
         init {
             // Define click listener for the ViewHolder's View.
             textView = view.findViewById(R.id.study_item_title)
+
+            //item 클릭시 웹뷰로 넘어감.
+            view.setOnClickListener {
+                val pos = absoluteAdapterPosition
+                val nextIntent = Intent(view.context, WebViewActivity::class.java)
+                Log.e("hi", pos.toString())
+                nextIntent.putExtra("url", dataSet[pos].url);
+                view.context.startActivity(nextIntent)
+            }
         }
     }
 
@@ -30,7 +40,7 @@ class StudyListAdapter(private val dataSet: ArrayList<AndroidStudyItem>) :
         val view = LayoutInflater.from(viewGroup.context)
             .inflate(R.layout.fragment_study_list_item, viewGroup, false)
 
-        return ViewHolder(view)
+        return ViewHolder(view, dataSet)
     }
 
     // Replace the contents of a view (invoked by the layout manager)
@@ -38,12 +48,10 @@ class StudyListAdapter(private val dataSet: ArrayList<AndroidStudyItem>) :
 
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
-//        viewHolder.textView.text = dataSet[position].title
         viewHolder.textView.text = dataSet[position].title
     }
 
     // Return the size of your dataset (invoked by the layout manager)
-//    override fun getItemCount() = dataSet.size
 
     override fun getItemCount() = dataSet.size
 }
---
+++ app/src/main/java/my/android_study/data/AndroidStudyList.kt
@@ -0,0 +1,11 @@
+package my.android_study.data
+
+import my.android_study.models.AndroidStudyItem
+
+object AndroidStudyList {
+    val androidStudyList = listOf(
+        AndroidStudyItem("1", "https://growing-software-engineer.tistory.com/14"),
+        AndroidStudyItem("2", "https://www.naver.com"),
+        AndroidStudyItem("3", "https://www.naver.com")
+    )
+}
