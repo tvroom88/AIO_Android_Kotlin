0f9385b0ee3c8135da6e337f54928987e9042c06
tvroom9102 <tvroom88@github.com>
2024-06-12 16:41:00 +0900

    1. BaseActivity에 Activity 간의 이동을 위한 공용 function 추가

--- app/src/main/java/com/aio/kotlin/MainActivity.kt
+++ app/src/main/java/com/aio/kotlin/MainActivity.kt
@@ -3,6 +3,7 @@ package com.aio.kotlin
 import android.os.Bundle
 import com.aio.kotlin.base.ViewBindingBaseActivity
 import com.aio.kotlin.databinding.ActivityMainBinding
+import com.aio.kotlin.jetpack.binding.databinding.DataBindingActivity
 
 class MainActivity : ViewBindingBaseActivity<ActivityMainBinding>() {
 
@@ -13,5 +14,7 @@ class MainActivity : ViewBindingBaseActivity<ActivityMainBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        moveActivityWithOnClick(mBinding.button, DataBindingActivity())
     }
 }
--- app/src/main/java/com/aio/kotlin/base/BaseActivity.kt
+++ app/src/main/java/com/aio/kotlin/base/BaseActivity.kt
@@ -1,6 +1,9 @@
 package com.aio.kotlin.base
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewbinding.ViewBinding
 
@@ -10,5 +13,16 @@ abstract class BaseActivity<VB : ViewBinding> : AppCompatActivity() {
         super.onCreate(savedInstanceState)
     }
 
-
+    /**
+     * View 클릭시 Activity를 이동시키는 기능이다.
+     *
+     * @param view  onClick할 View의 대상
+     * @param activity 이동할 activity
+     */
+    fun moveActivityWithOnClick(view: View, activity: Activity) {
+        view.setOnClickListener {
+            val intent = Intent(this, activity::class.java)
+            startActivity(intent)
+        }
+    }
 }
