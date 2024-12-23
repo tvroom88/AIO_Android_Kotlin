0c52d73e5617af6ea7a6cca8881f429ba4b6613a
tvroom9102 <tvroom88@github.com>
2024-12-23 14:35:24 +0900

    databinding 예제 추가

--- app/src/main/java/com/aio/kotlin/studylist/jetpack/binding/databinding/DataBindingViewModel.kt
+++ app/src/main/java/com/aio/kotlin/studylist/jetpack/binding/databinding/DataBindingViewModel.kt
@@ -1,10 +1,13 @@
 package com.aio.kotlin.studylist.jetpack.binding.databinding
 
-import android.widget.MultiAutoCompleteTextView
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.aio.kotlin.R
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class DataBindingViewModel : ViewModel() {
 
@@ -12,12 +15,19 @@ class DataBindingViewModel : ViewModel() {
     private val _basicText = MutableLiveData<String>()
     val basicText: LiveData<String> = _basicText
 
+    private val _username = MutableStateFlow<String>("")
+    val username: StateFlow<String> = _username
+
     var url = 0
 
     init {
         // 초기 데이터 설정
         _basicText.value = "Hello, DataBinding!"
         url = R.drawable.chick
+
+        viewModelScope.launch {
+            _username.value = loadUserName() // repository에서 가져왔다고 가정
+        }
     }
 
     fun resetText() {
@@ -28,4 +38,7 @@ class DataBindingViewModel : ViewModel() {
         return _basicText
     }
 
+    private fun loadUserName(): String {
+        return "Tom"
+    }
 }
